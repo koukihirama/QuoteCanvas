901b34864f63340a2fb92d2b8caff2ecf5e94093
平間洸揮 <hiramakouki@MacBook-Pro.local>
2025-08-23 19:35:28 +0900

    新規登録フォームの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def nav_link_to(name, path)
+    classes = "px-3"
+    classes << " font-semibold underline underline-offset-8" if current_page?(path)
+    link_to name, path, class: classes
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, length: { maximum: 30 }, allow_blank: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
