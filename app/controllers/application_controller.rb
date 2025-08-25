9ae5c7ab02d09a95322afac9ec534c741dbeb108
平間洸揮 <hiramakouki@MacBook-Pro.local>
2025-08-25 11:34:45 +0900

    ダッシュボードルーティングの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
+
+  #  ログイン後の遷移先
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  #  ログアウト後の遷移先
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
