4e0b14193e12d5d4e9893673bcc3526be0612f60
平間洸揮 <hiramakouki@MacBook-Pro.local>
2025-08-25 17:55:28 +0900

    ダッシュボードページの作成

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,14 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # Passageモデル と User#passages の両方があるときだけ触る
+    if defined?(Passage) && current_user.respond_to?(:passages)
+      scope = current_user.passages.order(created_at: :desc)
+      @recent_passages = scope.limit(3)
+      @passages_count  = scope.count
+    else
+      @recent_passages = []
+      @passages_count  = 0
+    end
   end
 end
