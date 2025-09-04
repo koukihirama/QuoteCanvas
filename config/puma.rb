22658d03b957fa8ffd3145b7396e3cf409de5e96
平間洸揮 <hiramakouki@MacBook-Pro.local>
2025-09-04 22:06:15 +0900

    renderdbを修正

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+require "active_record"
+
 workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
 threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
 threads threads_count, threads_count
@@ -7,12 +9,12 @@ port        ENV.fetch("PORT", 3000)
 environment ENV.fetch("RAILS_ENV", "production")
 pidfile     ENV.fetch("PIDFILE", "tmp/pids/server.pid")
 
+# 親プロセスで DB 切断
 before_fork do
-  # 親プロセスで掴んだ DB コネクションを必ず切る
-  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord::Base)
 end
 
+# 各ワーカー起動ごとに DB へ張り直し
 on_worker_boot do
-  # 子プロセス（各ワーカー）で DB に張り直す
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
 end
